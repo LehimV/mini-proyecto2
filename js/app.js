12029d8a89ed40c6131ea34c3621e2010fe01446
Lehim <lehim.vergara@gmail.com>
2023-04-11 21:53:43 -0600

    Iniciando

---
+++ js/app.js
@@ -0,0 +1,9 @@
+import data from "./data.js";
+
+//const URL = "./js/stays.json";
+
+/*const data = await data.getData(URL);
+
+console.log(data);*/
+
+data.forEach((element) => console.log(element));
---
+++ js/dom.js
@@ -0,0 +1,6 @@
+/* 
+Aqui van todas las funciones o variables relacionadas 
+con la manipulación del DOM en la aplicación
+*/
+
+//document.getElementById("myImg").src = "imagen.pdf";
